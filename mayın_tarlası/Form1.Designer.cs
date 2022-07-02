facf436fc612ce4c45cfdb81123eb5ce5b235b82
emineoren <oren2017@hotmail.com>
2022-07-03 01:31:47 +0300

    oyun

---
+++ mayın_tarlası/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace mayın_tarlası
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(759, 219);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.BackColor = System.Drawing.Color.SlateBlue;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Segoe Script", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(777, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ÜRET";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 8.25F);
+            this.label1.Location = new System.Drawing.Point(821, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "seçilen";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(793, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Script", 8.25F);
+            this.label2.Location = new System.Drawing.Point(796, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "bulunan mayın";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(796, 127);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 276);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ mayın_tarlası/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mayın_tarlası
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            butonüret();
+
+        }
+
+        private void butonüret()
+        {
+            flowLayoutPanel1.Controls.Clear();
+            Random rnd = new Random();
+            List<int> mayinlar = new List<int>();
+            for (int i = 1;mayinlar.Count <15; i++)
+            {
+                int sayi = rnd.Next(1, 61);
+                if (!mayinlar.Contains(sayi))
+                {
+                    mayinlar.Add(sayi);
+                }
+
+            }
+
+
+            for (int i =1; i < 61; i++)
+            {
+                Button btn = new Button();
+                btn.Size = new System.Drawing.Size(35, 35);
+                btn.Text = i.ToString();
+                if (mayinlar.Contains(i))
+                {
+                    btn.Tag = true;
+                }
+                else
+                {
+                    btn.Tag = false;       
+                }
+                btn.Click += Btn_Click;
+                flowLayoutPanel1.Controls.Add(btn);
+
+            }
+        }
+        int secilen = 0;
+        int bulunan = 0;
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button basilanButon = (Button)sender;
+            bool mayin = (bool)basilanButon.Tag;
+            secilen++;
+            if (mayin)
+            {
+                basilanButon.BackColor = Color.Red;
+                basilanButon.Enabled = false;
+                MessageBox.Show("Bravo! Mayını Buldun", "bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                bulunan++;
+                secilen--;
+            }
+            else
+            {
+                basilanButon.BackColor = Color.Blue;
+            }
+            textBox1.Text = secilen.ToString();
+            textBox2.Text = bulunan.ToString();
+            if (bulunan>=15)
+            {
+       DialogResult dialogResult = MessageBox.Show("Tüm mayınları buldun. devam etmek istermisin?", "bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                if (dialogResult==DialogResult.OK)
+                {
+                    butonüret();
+                    bulunan = 0;
+                    secilen = 0;
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
